76253000c6b8b5fdffb42ca1cc3a556291928e53
AlexanderLaaser <alexlaaser@hotmail.de>
2023-09-24 23:01:01 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// This is our zentral tsx file for adding React components
+import ListGroup from "./components/ListGroup.tsx";
+
+function App() {
+  return (
+    <div>
+      <ListGroup></ListGroup>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+// Thats a component
+function ListGroup() {
+  let listOfItems = ["Volleyball", "Gaming", "Club"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>Thats a List</h1>
+      {/*Normale Implementierung mit If/else*/}
+      {/*{listOfItems.length === 0 ? <p>No item found</p> : null}*/}
+      {listOfItems.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {listOfItems.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
